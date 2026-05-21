4072b05b28606c7c3b02d034a1c7c6e810a2a6af
Kelan <kelandesu@gmail.com>
2026-05-21 17:09:38 -0400

    Add department detail view and update templates for department listing

--- departments/urls.py
+++ departments/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.department_list, name='department_list'),
+    path('<int:department_id>/', views.department_detail, name='department_detail'),
 ]
--- departments/views.py
+++ departments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Department
 
 
@@ -6,6 +6,20 @@ def department_list(request):
     departments = Department.objects.all()
     return render(
         request,
-        'departments/department_list.html',
-        {'departments': departments}
+        "departments/department_list.html",
+        {"departments": departments}
+    )
+
+
+def department_detail(request, department_id):
+    department = get_object_or_404(Department, id=department_id)
+    course = department.courses.all()
+
+    return render(
+        request,
+        "departments/department_detail.html",
+        {
+            "department": department,
+            "courses": course
+        }
     )
