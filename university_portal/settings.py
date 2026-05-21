dd544c08209193c10719a62fddba667277f059a2
Kelan <kelandesu@gmail.com>
2026-05-21 15:50:56 -0400

    Update settings.py to configure PostgreSQL database and add application modules

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'departments',
+    'courses',
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'university_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'university_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Km15578!',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
