422ce68132de5082f4a6c3c5906527f3e4d48f3e
Kelan <kelandesu@gmail.com>
2026-06-08 09:32:16 -0400

    Add REST framework and django-filter configuration to settings

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'departments',
     'courses',
     'students',
+
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+
+    "PAGE_SIZE": 5,
+
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+}
